046d7f3cb49d0b1f3d3d764f41ebab44e2c1d409
harshitpal2 <harshitpal24022@gmail.com>
2025-05-03 14:58:52 +0530

    basic conversions in js

---
+++ conversion.js
@@ -0,0 +1,69 @@
+/*let scor="33sa"
+console.log(typeof scor);
+console.log(typeof(scor));
+let valueinnumbe=Number(scor)
+console.log(valueinnumbe);
+console.log(typeof valueinnumbe);
+ 
+let score=null
+console.log(typeof score);
+console.log(typeof(score));
+let valueinnumber=Number(score)
+console.log(valueinnumber);
+console.log(typeof valueinnumber);
+
+
+let core=undefined
+console.log(typeof core);
+console.log(typeof(core));
+let alueinnumber=Number(core)
+console.log(alueinnumber);
+console.log(typeof alueinnumber);
+
+
+let loggedIn= Boolean (0) ;
+console.log(loggedIn);
+
+*/
+//**************************** operation ********************************* */
+
+/*let value =3
+let negvalue= -value
+//console.log(negvalue);
+
+let str1="hello"
+let str2= " Harshit !"
+str3=str1+str2
+console.log(str3); 
+
+console.log("3"+2);
+console.log(3+"2");
+console.log("3"+2+2);
+console.log(3+2+"2");*/ 
+
+
+//************************************comparision operators *********** */
+
+//console.log(2>1);
+//console.log(2>=2);
+//console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+console.log(null==0);
+console.log(null>0);
+console.log(null<=0);
+
+console.log("2"===1);
+
+
+
+
+
+
+
+
+
+
+
+
